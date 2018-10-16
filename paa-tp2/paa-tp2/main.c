7a94fc728390d739171fef7e4bc12e16ad52a921
Gustavo Viegas <webviegas@gmail.com>
2018-10-16 10:19:56 -0300

    Commit inicial

---
+++ paa-tp2/paa-tp2/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  paa-tp2
+//
+//  Created by Gustavo Viegas on 16/10/18.
+//  Copyright © 2018 UFV Florestal. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
